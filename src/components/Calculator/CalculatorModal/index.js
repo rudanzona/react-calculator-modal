490d2c8ef09f555f1402b9e95d671c5a8d539346
maychen <rudanzona@gmail.com>
2020-07-06 14:46:25 +0800

    draggable calculator modal

--- src/components/Calculator/CalculatorModal/index.js
+++ src/components/Calculator/CalculatorModal/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactModal from 'react-modal'
+import Draggable from 'react-draggable'
+
 import Calculator from '../index'
 import './CalculatorModal.scss'
 
@@ -9,16 +11,30 @@ const CalculatorModal = (props) => {
     closeModal,
   } = props
 
+  const handleModalClose = (e) => {
+    if (e.target.className !== 'calculator-draggable-container') return
+
+    closeModal()
+  }
+
   return (
     <ReactModal
       isOpen={modalIsOpen}
       shouldCloseOnOverlayClick={true}
       onRequestClose={closeModal}
       contentLabel="Calculator Modal"
-      className="CalculatorContent"
-      overlayClassName="CalculatorOverlay"
+      className="calculator-content"
+      overlayClassName="calculator-overlay"
     >
-      <Calculator />
+      <div className="calculator-draggable-container"
+        onClick={handleModalClose}
+        >
+        <Draggable bounds="parent">
+          <div>
+            <Calculator />
+          </div>
+        </Draggable>
+      </div>
     </ReactModal>
   )
 }
